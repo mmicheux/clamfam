073d3b54383fe5d22ee873fb3310bbdf9778f3e2
Molly Micheux <mmicheux@gmail.com>
2019-01-24 20:04:33 -0500

    Add new clams, add gitignore

--- main.js
+++ main.js
@@ -11,17 +11,11 @@ let names = [
 	"kristen", "shelby",
 	"laurie", "livorio",
 	"shanley", "mary",
-	"kyle","patrick", "amanda","ayumi"
+	"kyle","patrick", "amanda","ayumi", "tom", "nina", "heather", "justin", "betsy","==="
 ];
 let colors = ['#71abdd','#6bcade','#82ccb5','#b6d884','#fff68f','#fdcd79','#f9b489','#f497aa','#dd86b9','#9977b4'];
-document.getElementById('names').innerText = 
-	names.sort().join('') + names.sort().join('') + names.sort().join('') 
-	+ names.sort().join('') + names.sort().join('')+ names.sort().join('') 
-	+ names.sort().join('') + names.sort().join('') + names.sort().join('')
-	+ names.sort().join('') + names.sort().join('') + names.sort().join('') 
-	+ names.sort().join('') + names.sort().join('') + names.sort().join('')
-	+ names.sort().join('') + names.sort().join('') + names.sort().join('') 
-	+ names.sort().join('') + names.sort().join('');
+document.getElementById('names').innerText = names.sort().join('').repeat(23)
+
 
 colors.forEach( color => document.getElementById('background').innerHTML += `<div class="rainbow" id="${color}" style="background-color:${color};"></div>` );
 

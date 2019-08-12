3e0198c07278cb13ac9a005c43cb35e598ab839e
Molly Micheux <mmicheux@gmail.com>
2019-08-12 11:08:22 -0400

    Fix Lori's spelling

--- main.js
+++ main.js
@@ -9,13 +9,20 @@ let names = [
 	"jojo", "ostin",
 	"zhue", "mary",
 	"kristen", "shelby",
-	"laurie", "livorio",
+	"lori", "livorio",
 	"shanley", "mary",
-	"kyle","patrick", "amanda","ayumi", "tom", "nina", "heather", "justin", "betsy","==="
+	"kyle","patrick", 
+	"amanda","ayumi", 
+	"tom", "nina", 
+	"heather", "justin", 
+	"betsy", "michelle",
+	"heather", "charlie",
+	"kennedy", "dre", "meg",
+	"emily", "brittney", 
+	"jackie", "patrick"
 ];
 let colors = ['#71abdd','#6bcade','#82ccb5','#b6d884','#fff68f','#fdcd79','#f9b489','#f497aa','#dd86b9','#9977b4'];
 document.getElementById('names').innerText = names.sort().join('').repeat(23)
-
 
 colors.forEach( color => document.getElementById('background').innerHTML += `<div class="rainbow" id="${color}" style="background-color:${color};"></div>` );
 
